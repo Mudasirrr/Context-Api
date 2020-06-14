308887eb0a23933251d61a988dc9a645eb9aa039
Mudasirrr <hmudasir714@gmail.com>
2020-06-14 03:23:35 -0400

    Updates

---
+++ precache-manifest.150770f10d6182a38c5d03b6892fb58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4be3a1cabfffc5d6cb8087c376181dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f27301e0998a839be7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbe112a523bb57e60ffe",
+    "url": "/static/js/2.f8ac9c34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8ac9c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f27301e0998a839be7",
+    "url": "/static/js/main.5d4fcb97.chunk.js"
+  },
+  {
+    "revision": "7477b319975604670abc",
+    "url": "/static/js/runtime-main.27638878.js"
+  }
+]);
